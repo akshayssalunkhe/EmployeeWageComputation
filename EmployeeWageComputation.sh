118980027cba409c2327558d359f81ed4ac75909
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-08 19:19:33 +0530

    REFACTORED:Added Function To Get Work Hours

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -13,29 +13,32 @@ WORKING_HRS_PER_MONTH=100;
 #VARIABLES
 salary=0;
 employeeHrs=0;
+totalEmployeeHrs=0;
 
+function getWorkingHours() 
+{
+	 case $1 in
+         $IS_WORKING_FULL_TIME)
+                  	employeeHrs=8
+                  	;;
+         $IS_WORKING_PART_TIME)
+                  	employeeHrs=4
+                  	;;
+         *)
+                  	employeeHrs=0
+                  	;;
+   esac
+
+echo $employeeHrs 
+}
 
 #USING LOOP TO VERIFY CONDITION
 for (( totalWorkingDays=1,totalEmployeeHrs=0;  totalWorkingDays<=20  &&  totalEmployeeHrs<=100 ; totalWorkingDays++ ))
 do
-  	employeeCheck=$((RANDOM%3))
+	employeeHrs="$( getWorkingHours $((RANDOM%3)) )"
+	totalEmployeeHrs=$(($totalEmployeeHrs+$employeeHrs))
 
-	#CHECKING CASE
-	case $employeeCheck in
-			$IS_WORKING_FULL_TIME)
-						employeeHrs=8
-						;;
-			$IS_WORKING_PART_TIME)
-						employeeHrs=4
-						;;
-			*)
-						employeeHrs=0
-						;;
-	esac
-
-totalEmployeeHrs=$(($totalEmployeeHrs + $employeeHrs))
-
-#CALCULATING DAILY SALARY AND ADDING IT TO GET MONTHLY SALARY
-salary=$(($EMPLOYEE_RATE_PER_HR * $employeeHrs))
-totalSalary=$(($totalSalary+$salary))
+	#CALCULATING DAILY SALARY AND ADDING IT TO GET MONTHLY SALARY
+	salary=$(($EMPLOYEE_RATE_PER_HR * $employeeHrs))
+	totalSalary=$(($totalSalary+$salary))
 done
