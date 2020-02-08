c97565f752705137174426da48507dcc85426cc8
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-08 15:39:33 +0530

    REFACTOR:ReplaceIfElse By Case

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -3,7 +3,7 @@
 #DISPLAYING WELCOME MESSAGE
 echo "Welcome To Employee Wage Computation Program"
 
-#CONSTANT
+#CONSTANTS
 IS_WORKING_FULL_TIME=1;
 IS_WORKING_PARTTIME=2;
 EMPLOYEE_RATE_PER_HR=20;
@@ -12,19 +12,23 @@ EMPLOYEE_HRS=8;
 #VARIABLES
 salary=0;
 employeeHrs=0;
+
 #GENERATING RANDOM VALUE 
 employeeCheck=$((RANDOM%3))
 
-#CHECKING CONDITION AND CALCULATING DAILY WAGE
-if [ $IS_WORKING_FULL_TIME -eq $employeeCheck ]
-then
-	employeeHrs=8;
-elif [ $IS_WORKING_PARTTIME -eq $employeeCheck ]
-then
-	employeeHrs=4;
-else
-	employeeHrs=0;
-fi
+#CHECKING CASE
+case $employeeCheck in
+		$IS_WORKING_FULL_TIME)
+					employeeHrs=8
+					;;
+		$IS_WORKING_PARTTIME)
+					employeeHrs=4
+					;;
+		*)
+					employeeHrs=0
+					;;
+esac
 
+#CALCULATING SALARY
 salary=$(($EMPLOYEE_RATE_PER_HR * $employeeHrs))
 echo " Daily Salary Of Employee = $salary "
