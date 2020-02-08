79308485cb3fd724d7a9996a88b256d2bb14ad3b
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-08 16:15:28 +0530

    ADDED:For Loop to calculate Monthly Salary

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -8,27 +8,32 @@ IS_WORKING_FULL_TIME=1;
 IS_WORKING_PARTTIME=2;
 EMPLOYEE_RATE_PER_HR=20;
 EMPLOYEE_HRS=8;
+WORKING_DAYS_PER_MONTH=20;
 
 #VARIABLES
 salary=0;
 employeeHrs=0;
+salaryOfMonth=0;
 
-#GENERATING RANDOM VALUE 
-employeeCheck=$((RANDOM%3))
+#USING LOOP TO GET MONTHLY SALARY
+for (( day=1; day<=$WORKING_DAYS_PER_MONTH; day++ ))
+do
+	employeeCheck=$((RANDOM%3))
 
-#CHECKING CASE
-case $employeeCheck in
-		$IS_WORKING_FULL_TIME)
-					employeeHrs=8
-					;;
-		$IS_WORKING_PARTTIME)
-					employeeHrs=4
-					;;
-		*)
-					employeeHrs=0
-					;;
-esac
+	#CHECKING CASE
+	case $employeeCheck in
+			$IS_WORKING_FULL_TIME)
+						employeeHrs=8
+						;;
+			$IS_WORKING_PARTTIME)
+						employeeHrs=4
+						;;
+			*)
+						employeeHrs=0
+						;;
+	esac
 
-#CALCULATING SALARY
+#CALCULATING DAILY SALARY AND ADDING IT TO GET MONTHLY SALARY
 salary=$(($EMPLOYEE_RATE_PER_HR * $employeeHrs))
-echo " Daily Salary Of Employee = $salary "
+totalSalary=$(($totalSalary+$salary))
+done
