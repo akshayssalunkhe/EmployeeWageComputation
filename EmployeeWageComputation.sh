c9e6afc4e8c5f82ac899bca1050e95787fa68ede
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-08 18:17:26 +0530

    ADDED:Condition In For Loop Of Total Working Hours And Days

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -5,33 +5,36 @@ echo "Welcome To Employee Wage Computation Program"
 
 #CONSTANTS
 IS_WORKING_FULL_TIME=1;
-IS_WORKING_PARTTIME=2;
+IS_WORKING_PART_TIME=2;
 EMPLOYEE_RATE_PER_HR=20;
-EMPLOYEE_HRS=8;
 WORKING_DAYS_PER_MONTH=20;
+WORKING_HRS_PER_MONTH=100;
 
 #VARIABLES
 salary=0;
 employeeHrs=0;
 salaryOfMonth=0;
+totalEmployeeHrs=0;
 
-#USING LOOP TO GET MONTHLY SALARY
-for (( day=1; day<=$WORKING_DAYS_PER_MONTH; day++ ))
+#USING LOOP TO VERIFY CONDITION
+for (( totalWorkingDays=1;  totalWorkingDays<=20  &&  totalEmployeeHrs<=100 ; totalWorkingDays++ ))
 do
-	employeeCheck=$((RANDOM%3))
+  	employeeCheck=$((RANDOM%3))
 
 	#CHECKING CASE
 	case $employeeCheck in
 			$IS_WORKING_FULL_TIME)
 						employeeHrs=8
 						;;
-			$IS_WORKING_PARTTIME)
+			$IS_WORKING_PART_TIME)
 						employeeHrs=4
 						;;
 			*)
 						employeeHrs=0
 						;;
 	esac
+
+totalEmployeeHrs=$(($totalEmployeeHrs + $employeeHrs))
 
 #CALCULATING DAILY SALARY AND ADDING IT TO GET MONTHLY SALARY
 salary=$(($EMPLOYEE_RATE_PER_HR * $employeeHrs))
